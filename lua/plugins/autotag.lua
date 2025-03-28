a737de78ccc894922dc17c049fc54f983a55bb4d
ManuGolem <manuelcarreras2013@gmail.com>
2025-03-28 13:39:57 -0300

    update

--- lua/plugins/autotag.lua
+++ lua/plugins/autotag.lua
@@ -1,13 +1,6 @@
 return {
-	{
-		"windwp/nvim-ts-autotag",
-		event = "InsertEnter", -- Cargar el plugin al entrar en el modo de inserción
-		config = function()
-			require("nvim-ts-autotag").setup({
-				enable_close = true, -- Habilitar el cierre automático de etiquetas
-				enable_rename = true, -- Habilitar el renombrado automático de etiquetas
-				enable_close_on_slash = false, -- Deshabilitar el cierre automático al usar '/'
-			})
-		end,
-	},
+	"tronikelis/ts-autotag.nvim",
+	opts = {},
+	-- ft = {}, optionally you can load it only in jsx/html
+	event = "VeryLazy",
 }
